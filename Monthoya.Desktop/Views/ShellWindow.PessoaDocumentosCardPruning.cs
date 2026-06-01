7f11cab785665e398bcfdb8e92b407a5d56db975
wjuniorbr2 <133127851+wjuniorbr2@users.noreply.github.com>
2026-06-01 04:51:20 -0300

    Show batch document information button

--- Monthoya.Desktop/Views/ShellWindow.PessoaDocumentosCardPruning.cs
+++ Monthoya.Desktop/Views/ShellWindow.PessoaDocumentosCardPruning.cs
@@ -88,6 +88,7 @@ public partial class ShellWindow
         RemovePessoaDocumentosGridColumn("Caminho");
 
         ConfigurePessoaDocumentoArquivoSelector();
+        EnsurePessoaDocumentosBatchUseDataButton();
         UpdatePessoaDocumentoEditorAvailability();
     }
 
