d1cc5944f49b6df030255d374a52b5176458fa01
wjuniorbr2 <wjuniorbr@hotmail.com>
2026-06-11 00:21:54 -0300

    Extract pessoa label helpers

--- Monthoya.Data/RentalManagement/RentalManagementService.Labels.cs
+++ Monthoya.Data/RentalManagement/RentalManagementService.Labels.cs
@@ -4,37 +4,6 @@ namespace Monthoya.Data.RentalManagement;
 
 public sealed partial class RentalManagementService
 {
-    private static string GetPessoaRolesLabel(bool isProprietario, bool isLocatario, bool isFiador)
-    {
-        var roles = new List<string>();
-        if (isProprietario) roles.Add("Proprietário");
-        if (isLocatario) roles.Add("Locatário");
-        if (isFiador) roles.Add("Fiador");
-        return roles.Count == 0 ? "-" : string.Join(", ", roles);
-    }
-
-    private static string GetPessoaDocumentoTipoLabel(string tipo) =>
-        tipo switch
-        {
-            "cpf" => "CPF",
-            "rg" => "RG",
-            "comprovante_residencia" => "Comprovante de residência",
-            "comprovante_renda" => "Comprovante de renda",
-            "estado_civil" => "Comprovante de estado civil",
-            "contrato_social" => "Contrato social",
-            "cartao_cnpj" => "Cartão CNPJ",
-            "procuracao" => "Procuração/autorização",
-            "dados_bancarios" => "Dados bancários",
-            _ => "Outros"
-        };
-
-    private static string GetDocumentoDeLabel(string documentoDe) =>
-        documentoDe switch
-        {
-            "conjuge" => "Cônjuge",
-            "empresa_trabalho" => "Empresa onde trabalha",
-            _ => "Pessoa"
-        };
 
     private static string GetImovelPublicacaoLabel(Imovel imovel) =>
         GetImovelPublicacaoLabel(imovel.PublicarNoSite, imovel.PublicarNoApp, imovel.Destaque);
