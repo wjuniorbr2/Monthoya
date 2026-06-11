a82d11f5de181e467559f7e3bca50088dd9b5a64
wjuniorbr2 <wjuniorbr@hotmail.com>
2026-06-11 00:25:28 -0300

    Extract vistoria label helpers

--- Monthoya.Data/RentalManagement/RentalManagementService.Labels.cs
+++ Monthoya.Data/RentalManagement/RentalManagementService.Labels.cs
@@ -7,29 +7,5 @@ public sealed partial class RentalManagementService
 
 
 
-    private static string GetVistoriaTipoLabel(VistoriaTipo tipo) =>
-        tipo switch
-        {
-            VistoriaTipo.InicialProprietario => "Inicial do proprietário",
-            VistoriaTipo.Entrada => "Entrada da locação",
-            VistoriaTipo.Saida => "Saída da locação",
-            VistoriaTipo.Periodica => "Periódica",
-            VistoriaTipo.Manutencao => "Manutenção",
-            VistoriaTipo.Outros => "Outra",
-            _ => tipo.ToString()
-        };
-
-    private static string GetVistoriaStatusLabel(VistoriaStatus status) =>
-        status switch
-        {
-            VistoriaStatus.Draft => "Rascunho",
-            VistoriaStatus.InProgress => "Em andamento",
-            VistoriaStatus.ReadyToReview => "Pronta para revisão",
-            VistoriaStatus.Finished => "Finalizada",
-            VistoriaStatus.SignedPaper => "Assinada em papel",
-            VistoriaStatus.SignedDigitally => "Assinada digitalmente",
-            VistoriaStatus.Canceled => "Cancelada",
-            _ => status.ToString()
-        };
 }
 
