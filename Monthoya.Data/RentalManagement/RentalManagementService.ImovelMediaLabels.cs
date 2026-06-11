380037a7d305171dd7ad422342fcafe3ede6d662
wjuniorbr2 <wjuniorbr@hotmail.com>
2026-06-11 00:24:18 -0300

    Extract imovel media label helpers

---
+++ Monthoya.Data/RentalManagement/RentalManagementService.ImovelMediaLabels.cs
@@ -0,0 +1,27 @@
+﻿using Monthoya.Core.Entities;
+
+namespace Monthoya.Data.RentalManagement;
+
+public sealed partial class RentalManagementService
+{
+    private static string GetImovelMediaCategoryLabel(ImovelMediaCategory category) =>
+        category switch
+        {
+            ImovelMediaCategory.PropertyPhoto => "Foto pública do imóvel",
+            ImovelMediaCategory.Document => "Documento",
+            ImovelMediaCategory.InspectionPhoto => "Foto de vistoria",
+            ImovelMediaCategory.MaintenancePhoto => "Foto de manutenção",
+            ImovelMediaCategory.Other => "Outro",
+            _ => category.ToString()
+        };
+
+    private static string GetImovelMediaSourceLabel(ImovelMediaSource source) =>
+        source switch
+        {
+            ImovelMediaSource.Windows => "Windows",
+            ImovelMediaSource.AndroidStaff => "Android equipe",
+            ImovelMediaSource.Website => "Site",
+            ImovelMediaSource.Import => "Importação",
+            _ => source.ToString()
+        };
+}
